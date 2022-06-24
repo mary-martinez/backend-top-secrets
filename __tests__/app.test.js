5dc995362e3aa7084b1984b558111eefc1db86bb
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-24 14:11:14 -0700

    test is set up for creating a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { get } = require('../lib/app');
+
+const mockUser = {
+  first_name: 'Test',
+  last_name: 'User',
+  email: '123@test.com',
+  password: 123456
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('POST /api/v1/users should create a new user', async () => {
+    const res = await app().post('/api/v1/users').send(mockUser);
+    const { first_name, last_name, email } = mockUser;
+    expect(res.body).toContain({ first_name, last_name, email });
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if EXISTS users ;
+
+CREATE table users(
+  id BIGINT GENERATED ALWAYS as IDENTITY PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  password_hash VARCHAR NOT NULL
+);
