ea24ff6d2a6f09770e1025785a0fb89655a2be7b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-24 20:53:57 -0700

    test for authentication is red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,24 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const { get } = require('../lib/app');
+const { UserService } = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
   lastName: 'User',
   email: '123@test.com',
   password: '123456'
+};
+
+const signUpAndIn = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  console.log('user', { user });
+  return [agent, user];
 };
 
 describe('backend-express-template routes', () => {
@@ -23,7 +35,15 @@ describe('backend-express-template routes', () => {
   it('/api/v1/users/sessions signs in a user', async () => {
     await request(app).post('/api/v1/users').send(mockUser);
     const res = await request(app).post('/api/v1/users/sessions').send({ email: '123@test.com', password: '123456' });
-    console.log('res.body', res.body);
+    expect(res.status).toEqual(200);
+  });
+  it('/protected should return a 401 if not authenticated', async () => {
+    const res = await request(app).get('/api/v1/users/protected');
+    expect(res.status).toEqual(401);
+  });
+  it('/protected should return the current user if authenticated', async () => {
+    const [agent] = await signUpAndIn();
+    const res = await agent.get('/api/v1/users/protected');
     expect(res.status).toEqual(200);
   });
   afterAll(() => {
