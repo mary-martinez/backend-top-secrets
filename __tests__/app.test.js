6f6737383b8f2b003b6a65c27b6045c39254cc63
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-24 22:19:00 -0700

    tests for authenticate are passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { get } = require('../lib/app');
 const { UserService } = require('../lib/services/UserService');
 
 const mockUser = {
